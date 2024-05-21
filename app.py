68df94a964b7b65ecfecd81dd557c60425f42daa
Myles Ingram <143450175+mylesai25@users.noreply.github.com>
2024-05-21 15:34:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ if os.environ['OPENAI_API_KEY'] and uploaded_file:
             st.markdown(prompt)
         # Display assistant response in chat message container
         with st.spinner(text='Thinking'):
-                stream = agent_with_chat_history.invoke({'input':prompt}, config={'configurable': {'session_id': memory}})
+                stream = agent_with_chat_history.invoke({'input':prompt}, config={'configurable': {'session_id': "<foo>"}})
         with st.chat_message("assistant"):
                 response = stream['output']
                 st.write(response)
