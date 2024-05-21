12b4e8e30bd2a730360d17105ec6f8ce05d1b2bb
Myles Ingram <143450175+mylesai25@users.noreply.github.com>
2024-05-21 15:03:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -84,5 +84,5 @@ if os.environ['OPENAI_API_KEY'] and uploaded_file:
         # Display assistant response in chat message container
         with st.chat_message("assistant"):
             stream = agent_with_chat_history.stream({'input':prompt}, config={'configurable': {'session_id': 'test-session'}})
-            response = st.write_stream(stream)
+            response = st.write_stream(stream['messages'].content)
         st.session_state.messages.append({"role": "assistant", "content": response})
