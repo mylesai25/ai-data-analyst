0a23db775341dcfaf0c0acf96adfdad8cff892ec
Myles Ingram <143450175+mylesai25@users.noreply.github.com>
2024-05-28 09:50:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ if os.environ['OPENAI_API_KEY'] and uploaded_file:
         # )
         transcription = client.audio.transcriptions.create(
           model="whisper-1", 
-          file=wave_io.read()
+          file=wav_io.read()
         )
         st.write(transcription.text)
 
