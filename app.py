ba42b669419830180a71a2a7e60a7eaed49d6e4e
Myles Ingram <143450175+mylesai25@users.noreply.github.com>
2024-05-28 07:25:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ if os.environ['OPENAI_API_KEY'] and uploaded_file:
     @st.cache_resource
     def create_file(uploaded_file):
       file = client.files.create(
-        file=uploaded_file
+        file=uploaded_file,
         purpose='assistants'
       )
       return file
